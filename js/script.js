beff40af35dcbe99c0f70305a10422cdf750c0aa
AleAlle62 <basketallegrini@gmail.com>
2022-11-05 17:24:51 +0100

    3/5

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 passaggi:
 
-1. creazione array di numeri casuali da 1 a 100
-2. creazione di un timer di 30 secondi
+1. creazione array di numeri casuali da 1 a 100            OK!
+2. creazione di un timer di 30 secondi             OK!
 3. finito il timer l'array scompare 
-4. chiedere tramite prompt, i numeri dell'array
+4. chiedere tramite prompt, i numeri dell'array             OK!
 5. confrontare i numeri inseriti con quelli generati 
 6. dire quanti sono uguali  
 */
@@ -23,10 +23,10 @@ let numeriHtml = document.querySelector('.numeri-casuali')
 //dichiarazione array
 let arrayRandom = [];
 // ciclo che dice per quanti numeri viene ripetuto
-for (var i = 0; i < 6 ; i++){
+for (var i = 0; i < 5 ; i++){
     //creazione dei numeri random
-    let numberRandom = Math.floor(Math.random()*100)+1;
     let result = true;
+    let numberRandom = Math.floor(Math.random()*100)+1;
     for (var j = 0; j < i; j++){
         if (arrayRandom [j] == numberRandom)result =false;
     }
@@ -36,10 +36,40 @@ for (var i = 0; i < 6 ; i++){
         i--;
     }
 }
-// console.log(arrayRandom)
+console.log(arrayRandom)
 //stampa dei numeri casuali in pagina 
 numeriHtml.innerHTML = `<div class=".numeri-casuali">${arrayRandom}</div>`
 
 
 
 // 2. CREAZIONE DEL TIMER DI 30 SECONDI 
+//creazione del tempo di partenza per il counter
+let counter = 5;
+
+// intervallo di tempo per cui cambia il contdown 
+let idInterval = setInterval(count, 1000);
+
+// creazione funzione del counter, con all'interno la funzione che se arriva a zero fa il prompt
+function count() {
+	if (counter === 0) {
+
+        // COME CANCELLARE I NUMERI CASUALI?
+        
+
+
+
+
+        // stampa in console
+		console.log('scaduto il tempo per memorizzare')
+        // stampa la domanda dei numeri da chiedere con un ciclo di 5 volte
+        for (let i = 0; i < 5; i++){
+            prompt('insersci i numeri che ti ricordi')
+        }
+		clearInterval(idInterval)
+        //far sparire l'array di numeri
+
+	} else {
+		console.log(counter)
+		counter--
+	}
+}
